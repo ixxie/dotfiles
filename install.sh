578d495cbaa95b5e572b42553a45f8bd29b91a60
llgtr <llgtr@users.noreply.github.com>
2017-06-25 18:12:05 +0300

    Refactor and clean install.sh
    Some pointers taken from Google's shell style guide.

--- install.sh
+++ install.sh
@@ -4,13 +4,34 @@ dot_root=$(pwd -P)
 
 set -e
 
-function link_file() {
+##################################################
+# Helper function to write out in color
+# Arguments:
+#   $1: String that is written out
+#   $2: tput foreground color
+#   $3: Flag -b for bold text
+##################################################
+function echo_c {
+    if [[ $3 == "-b" ]]; then
+        echo "$(tput bold && tput setaf $2)$1$(tput sgr0)"
+    else
+        echo "$(tput setaf $2)$1$(tput sgr0)"
+    fi
+}
+
+##################################################
+# Function for linking files and directories.
+# Makes sure that there are no conflicts.
+# Arguments:
+#   None
+##################################################
+function link_file {
     local src="$dot_root/$1"
     local dst="$HOME/$2"
 
     # Check for directory.
     if [ ! -e "${dst%/*}" ]; then
-        read -r -p "$(tput setaf 3)No directory at target. Create a directory for '$dst' [y/n]? $(tput sgr0)" prompt
+        read -r -p "$(echo_c "No directory at target. Create a directory for '$dst' [y/n]? " 3)" prompt
         if [[ ! $prompt =~ ^[Yy]$ ]]; then
             return
         fi
@@ -25,14 +46,14 @@ function link_file() {
 
     # Check if overwriting directory or file.
     if [ -d "$dst" ]; then
-        read -r -p "$(tput setaf 3)Directory '$dst' already exists. Do you wish to overwrite it [y/n]? $(tput sgr0)" prompt
+        read -r -p "$(echo_c "Directory '$dst' already exists. Do you wish to overwrite it [y/n]? " 3)" prompt
         if [[ ! $prompt =~ ^[Yy]$ ]]; then
             return
         fi
 
         rm -rf "$dst"
     elif [ -e "$dst" ]; then
-        read -r -p "$(tput setaf 3)File '$dst' already exists. Do you wish to overwrite it [y/n]? $(tput sgr0)" prompt
+        read -r -p "$(echo_c "File '$dst' already exists. Do you wish to overwrite it [y/n]? " 3)" prompt
         if [[ ! $prompt =~ ^[Yy]$ ]]; then
             return
         fi
@@ -40,13 +61,17 @@ function link_file() {
         rm "$dst"
     fi
 
-    # Create symbolic link.
-    echo "$(tput setaf 6)Symlinking$(tput sgr0) $dst"
+    echo "$(echo_c "Symlinking" 6) $dst"
     ln -s "$src" "$dst"
 }
 
 # Taken from https://github.com/holman/dotfiles/blob/master/script/bootstrap
-setup_gitconfig () {
+##################################################
+# Sets up git name and email to '.gitconfig.local'
+# Arguments:
+#   None
+##################################################
+function setup_gitconfig {
   if [ ! -f git/.gitconfig.local ]; then
     git_credential='cache'
 
@@ -54,14 +79,18 @@ setup_gitconfig () {
       git_credential='osxkeychain'
     fi
 
-    read -r -p "$(tput setaf 3)What is your GitHub author name? $(tput sgr0)" git_authorname
-
-    read -r -p "$(tput setaf 3)What is your github author email? $(tput sgr0)" git_authoremail
+    read -r -p "$(echo_c "What is your git user.name? " 3)" git_authorname
+    read -r -p "$(echo_c "What is your git user.email? " 3)" git_authoremail
 
     sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/.gitconfig.local.example > git/.gitconfig.local
   fi
 }
 
+##################################################
+# Links the common files and directories.
+# Arguments:
+#   None
+##################################################
 function linker {
     link_file "sh/.bash_profile" ".bash_profile"
     link_file "sh/.bashrc" ".bashrc"
@@ -76,22 +105,27 @@ function linker {
     link_file "vim/colors/base16-eighties.vim" ".vim/colors/base16-eighties.vim"
 }
 
-echo "$(tput bold && tput setaf 2)Setting up .gitconfig$(tput sgr0)"
+echo_c "Setting up .gitconfig" 2 -b
 setup_gitconfig
-echo "$(tput bold && tput setaf 2)Linking non-specific files.$(tput sgr0)"
+
+echo_c "Linking common files." 2 -b
 linker
-echo "$(tput bold && tput setaf 2)Initializing submodules.$(tput sgr0)"
+
+echo_c "Initializing submodules." 2 -b
 git submodule update --init --recursive
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "$(tput bold &&tput setaf 2)Running OSX-specific scripts.$(tput sgr0)"
+    echo_c "Running MacOS-specific stuff." 2 -b
+
     link_file "tmux/.tmux-osx.conf" ".tmux.conf"
-    read -r -p "$(tput setaf 3)Run defaults.sh? $(tput sgr0)" prompt
+
+    read -r -p "$(echo_c "Run defaults.sh? " 3)" prompt
     if [[ $prompt =~ ^[Yy]$ ]]; then
         sh ./osx/defaults.sh
     fi
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "$(tput bold && tput setaf 2)Running Linux-specific scripts.$(tput sgr0)"
+    echo_c "Running Linux-specific stuff." 2 -b
+
     link_file "tmux/.tmux-linux.conf" ".tmux.conf"
     link_file "linux/xfce-term/" ".config/xfce4/terminal"
     link_file "linux/openbox/" ".config/openbox"
@@ -102,12 +136,13 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     link_file "linux/i3/" ".config/i3"
     link_file "linux/polybar/" ".config/polybar"
     link_file "linux/compton/compton.conf" ".config/compton.conf"
+
     if [[ -f /etc/arch-release ]]; then
-        read -r -p "$(tput setaf 3)Run pacman.sh? $(tput sgr0)" prompt
+        read -r -p "$(echo_c "Run pacman.sh " 3)" prompt
         if [[ $prompt =~ ^[Yy]$ ]]; then
             sh ./linux/scripts/pacman.sh
         fi
     fi
 fi
-echo "$(tput bold && tput setaf 2)Done."$(tput sgr0)
 
+echo_c "Done." 2 -b
