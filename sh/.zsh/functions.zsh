76d9f940395dd5d6ae100c388135642024d74b89
llgtr <llgtr@users.noreply.github.com>
2017-06-29 09:58:43 +0300

    Update functions.zsh
    - echo_c for easier colored strings in shell
    - timer for misc timing needs

--- sh/.zsh/functions.zsh
+++ sh/.zsh/functions.zsh
@@ -1,3 +1,30 @@
+# Zsh hook for the urxvt title bar
 preexec () {
     print -Pn "\e]2;urxvt :: $1\a"
+}
+
+# Makes colorizing output a bit easier.
+# $1: string to be colorized
+# $2: tput color code
+# $3: flag for bold text
+echo_c () {
+    if [[ $3 == "-b" ]]; then
+        echo "$(tput bold && tput setaf $2)$1$(tput sgr0)"
+    else
+        echo "$(tput setaf $2)$1$(tput sgr0)"
+    fi
+}
+
+# Timer for various simple timing needs.
+# $1: time in seconds (mandatory)
+timer () {
+    if [[ $1 == "" ]]; then
+        echo "$(echo_c "ERROR:" 1 -b) Missing parameter"
+        return
+    elif ! [[ $1 =~ '^[0-9]+$' ]]; then
+        echo "$(echo_c "ERROR:" 1 -b) Not a number"
+        return
+    fi
+
+    for i in {1..$1}; do sleep 1; echo -n -e $i\\r; done
 }
