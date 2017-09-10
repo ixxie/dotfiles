e4986b050b8cb8e255e4c7bb07784a8509128e90
llgtr <llgtr@users.noreply.github.com>
2017-09-10 19:08:24 +0300

    Remove auto-generated stuff

--- emacs/init.el
+++ emacs/init.el
@@ -30,17 +30,4 @@
 ;; Load configurations
 (require 'init-packages)
 (require 'init-misc-configs)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters which-key telephone-line rust-mode rainbow-mode intero clojure-mode base16-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
