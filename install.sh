fd381ef8723a3bcd82a4c38e2b40356db8beefab
Matan Shenhav <matan.shenhav@sievo.com>
2017-09-19 15:39:24 +0300

    Used dirname rather than relative paths to create symlinks in install.sh.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+dotfiles = dirname $0
 
-ln -s ./nix/home.nix ~/.config/nixpkgs/home.nix
-ln -s ./nix/config.nix ~/.config/nixpkgs/config.nix
-ln -s ./nix/home-manager ~/.config/nixpkgs/home-manager
+ln -s ${dotfiles}/nix/home.nix ~/.config/nixpkgs/home.nix
+ln -s ${dotfiles}/nix/config.nix ~/.config/nixpkgs/config.nix
+ln -s ${dotfiles}/nix/home-manager ~/.config/nixpkgs/home-manager
 
