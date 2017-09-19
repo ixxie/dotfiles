cfeecdce8ebbac5968c479bce204d8c72115d0b5
Matan Shenhav <matan.shenhav@sievo.com>
2017-09-19 16:01:21 +0300

    Added git submodule init & update to install.sh to fetch home-manager.

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ ln -s ${dotfiles}/nix/home.nix ~/.config/nixpkgs/home.nix
 ln -s ${dotfiles}/nix/config.nix ~/.config/nixpkgs/config.nix
 ln -s ${dotfiles}/nix/home-manager ~/.config/nixpkgs/home-manager
 
+git submodule init
+git submodule update
