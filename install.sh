78590b941494ce43dde2bf9ba9a22edcd5a51b1d
Matan Shenhav <matan.shenhav@sievo.com>
2017-09-19 12:58:49 +0300

    Added install script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+ln -s ./nix/nixpkgs/home.nix ~/.config/nixpkgs/home.nix
+ln -s ./nix/nixpkgs/config.nix ~/.config/nixpkgs/config.nix
+ln -s ./nix/nixpkgs/home-manager ~/.config/nixpkgs/home-manager
+
