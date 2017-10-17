d21b7ae753958fc321f09fe73f3e1e5887d7994e
Matan Shenhav <matan.shenhav@sievo.com>
2017-10-17 21:54:33 +0300

    Updated installer to create missing directories and support the latest HM version.

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
+# Identify dotfiles path by finding this script's directory
 dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s ${dotfiles}/nix/home.nix ~/.config/nixpkgs/home.nix
-ln -s ${dotfiles}/nix/config.nix ~/.config/nixpkgs/config.nix
-ln -s ${dotfiles}/nix/home-manager ~/.config/nixpkgs/home-manager
+# Ensure a directory exists
+function direx
+{
+    if [ ! -d $1 ]; then
+        if [ ! -d dirname $1 ]; then
+            direx dirname $1
+        fi
+        mkdir $1
+    fi
+}
+
+# Make a symbolic link, ensuring the directory exists
+function link
+{
+    direx dirname $2
+    ln -s $1 $2
+}
+
+# Link the home-manager:
+link ${dotfiles}/nix/home-manager ~/.config/nixpkgs/home-manager
+link ${dotfiles}/nix/home-manager/overlay.nix ~/.config/nixpkgs/overlays/home-manager.nix
+link ${dotfiles}/nix/home.nix ~/.config/nixpkgs/home.nix
+link ${dotfiles}/nix/config.nix ~/.config/nixpkgs/config.nix
+
 
 git submodule init
 git submodule update
