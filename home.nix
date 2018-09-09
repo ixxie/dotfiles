62b25895469871bec9b603c660e5c85b3367058e
Matan Shenhav <matan@fluxcraft.net>
2018-09-09 12:16:47 +0300

    Remove emacs

--- home.nix
+++ home.nix
@@ -1,75 +1,40 @@
 { pkgs, ... }:
  
 {
-   home.packages = with pkgs;
-   [
-       emacs25-nox
-       emacs-all-the-icons-fonts
-       python36Packages.flake8
-   ];
-
-    home.file.".tmux.conf".source = ../tmux/tmux.conf;
+    
+  home.file.".tmux.conf".source = ./tmux/tmux.conf;
  
-    home.file.".emacs.d/init.el".source = ../emacs/init.el;
-    home.file.".emacs.d/init".source = ../emacs/init;
-  
-    programs =
-    {
-            emacs =
-            {
-                enable = true;
-                extraPackages = epkgs: with epkgs;
-                [
-                    base16-theme
-                    evil
-                    magit
-                    multi-term
-                    nix-mode
-                    projectile
-                    rainbow-mode
-                    smartparens
-                    neotree 
-                    tabbar
-                    telephone-line
-                    use-package
-                    which-key
-                    flycheck
-                    flycheck-pycheckers
-                    intero
-                    haskell-mode
-                ];
-            };
- 
-            zsh =
-            {
-                enable = true;
- 
-            };
- 
-            git =
-            {
-                enable = true;
-                
-                userName = "Matan Shenhav";
-                userEmail = "matan@fluxcraft.net";
+  programs = {
 
-                extraConfig =
-                    ''
-                    [color "branch"]
-                        current = green bold
-                        local = green
-                        remote = yellow
-
-                    [color "diff"]
-                        frag = cyan bold
-                        meta = yellow bold
-                        new = green
-                        old = red
-
-                    [diff "bin"]
-                        textconv = hexdump -v -C
-
-                    '';
-            };
+    home-manager = {
+      enable = true;
+      path = "/home/ixxie/nixdev/home-manager";
     };
+
+    zsh.enable = true;
+
+    git = {
+
+      enable = true;
+	
+      userName = "Matan Shenhav";
+      userEmail = "matan@fluxcraft.net";
+
+      extraConfig = ''
+	[color "branch"]
+	  current = green bold
+	  local = green
+	  remote = yellow
+
+	[color "diff"]
+	  frag = cyan bold
+	  meta = yellow bold
+	  new = green
+	  old = red
+
+	[diff "bin"]
+	  textconv = hexdump -v -C
+      '';
+    };
+  };
 }
