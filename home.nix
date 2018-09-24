ff70d34759abc68ab4e8869b4f92cdecbe80f66a
Matan Shenhav <matan@fluxcraft.net>
2018-09-24 15:12:04 +0300

    Add support for truecolor in Tmux & Neovim

--- home.nix
+++ home.nix
@@ -81,8 +81,12 @@ in
 
           " Appearance "
           """"""""""""""
-
+          
+          let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+          let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
           set termguicolors
+
+          
           colorscheme freshcut
 
           " cool themes:
