15c88775780928f3dcb0a85e5b93a84d75ed2c24
Matan Shenhav <matan@fluxcraft.net>
2020-07-18 09:39:37 +0300

    Update home-manage URL

--- home.nix
+++ home.nix
@@ -32,7 +32,8 @@ in
 
     home-manager = {
       enable = true;
-      path = "https://github.com/rycee/home-manager/archive/release-20.03.tar.gz";
+      path = "https://github.com/rycee/home-manager/archive/master.tar.gz";
+    };
     };
 
     neovim = {
