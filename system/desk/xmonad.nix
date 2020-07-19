7ffb975fe6169d06ce2bdb98b93c15a513ae4c97
Matan Shenhav <matan@fluxcraft.net>
2020-07-19 15:10:27 +0300

    Add basic xmonad configuration

--- system/desk/xmonad.nix
+++ system/desk/xmonad.nix
@@ -6,6 +6,14 @@
       xmonad = {
         enable = true;
         enableContribAndExtras = true;
+        config = ''
+          import XMonad
+          
+          main = do
+            xmonad $ defaultConfig
+              { modMask = mod4Mask     -- Rebind Mod to the Windows key
+              }
+        '';
       };
     };
   };
