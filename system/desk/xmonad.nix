bdb4a5c97de618e31e9270ebba76eeb6bae6f6d9
Matan Shenhav <matan@fluxcraft.net>
2020-07-19 23:53:44 +0300

    Add picom compositor to xmonad module
    Video was producing artifacts in xmonad, and it turns out this is due to
    lack of a compositor offered by desktop environments such as gnome.

--- system/desk/xmonad.nix
+++ system/desk/xmonad.nix
@@ -1,20 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver = {
-    windowManager = {
-      xmonad = {
-        enable = true;
-        enableContribAndExtras = true;
-        config = ''
-          import XMonad
-          
-          main = do
-            xmonad $ defaultConfig
-              { modMask = mod4Mask     -- Rebind Mod to the Windows key
-              }
-        '';
+  services =  {
+    xserver = {
+      windowManager = {
+        xmonad = {
+          enable = true;
+          enableContribAndExtras = true;
+          config = ''
+            import XMonad
+            
+            main = do
+              xmonad $ defaultConfig
+                { modMask = mod4Mask     -- Rebind Mod to the Windows key
+                }
+          '';
+        };
       };
     };
+    picom.enable = true;
   };
 }
