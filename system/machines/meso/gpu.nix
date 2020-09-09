e3f02ce00fb5ee1979fd696c2a42b758637c6aae
Matan Shenhav <matan@fluxcraft.net>
2020-09-09 23:42:38 +0300

    Simplify GPU config

--- system/machines/meso/gpu.nix
+++ system/machines/meso/gpu.nix
@@ -1,22 +1,7 @@
 { config, pkgs, ... }:
-let
-  vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  openglPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl ];
-in {
-  environment.systemPackages = with pkgs; [
-    xorg.xbacklight
-    xorg.xf86videointel
-  ];
 
-  hardware.opengl = {
-    extraPackages32 = openglPackages;
-    extraPackages = openglPackages;
-  };
-  services.xserver = {
-    useGlamor = true;
-    deviceSection = ''
-      Option "DRI" "2"
-      Option "TearFree" "True"
-    '';
-  };
+{
+  environment.systemPackages = with pkgs; [
+    intel-media-driver
+  ];
 }
