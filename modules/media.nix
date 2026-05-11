65d5c844e0d19852522450ddb35feef074443725
Matan Bendix Shenhav <matan@shenhav.fyi>
2026-05-11 23:04:36 +0200

    feat(media): enable bluetooth audio enhancements
    Teach wireplumber about SBC-XQ, mSBC, hardware volume, and the full set of A2DP/BAP/HSP/HFP roles so bluetooth headsets get higher-quality codecs and proper mic support.

--- modules/media.nix
+++ modules/media.nix
@@ -13,6 +13,23 @@
     jack.enable = true;
     alsa.enable = true;
     wireplumber.enable = true;
+    wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "a2dp_sink"
+          "a2dp_source"
+          "bap_sink"
+          "bap_source"
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
   };
   services.pulseaudio.enable = false;
 }
